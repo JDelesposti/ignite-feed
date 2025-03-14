c336334ed830f24b65b5fdb7cb44bdf4cfd5271e
Junior Delesposti <96741873+JDelesposti@users.noreply.github.com>
2025-03-14 19:50:02 +0000

    feat: add props and params to Post component and tweak sidebar img styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
 import { PencilLine } from "phosphor-react";
-import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -11,9 +10,9 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={styles.profile}>
-        <Avatar src="https://github.com/JDelesposti.png" />
+        <img className={styles.avatar} src="https://github.com/JDelesposti.png" />
         <strong>Junior Delesposti</strong>
-        <span>Web Developer</span>
+        <span>CEO @TechnoSH</span>
       </div>
 
       <footer>
